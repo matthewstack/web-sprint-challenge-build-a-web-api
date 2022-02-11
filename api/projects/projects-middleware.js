815b873f66834625309e9785ace64e6350c4305e
Matt <86365526+matthewstack@users.noreply.github.com>
2022-02-11 11:51:44 -0500

    middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,38 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model");
+
+function logger(req, res, next) {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+}
+
+async function validateProject(req, res, next) {
+  const { id } = req.params;
+  try {
+    const existingProject = await Projects.get(id);
+    if (!existingProject) {
+      next({ status: 404, message: `project not found` });
+    } else {
+      req.project = existingProject;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+function validateProjectPayload(req, res, next) {
+  const { name, description, completed } = req.body;
+
+  if (name && description && (completed === false || completed === true)) {
+    next();
+  } else {
+    next({ status: 400, message: "missing required field" });
+  }
+}
+
+module.exports = {
+  logger,
+  validateProject,
+  validateProjectPayload,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require("express");
+const router = express.Router();
+
+const Projects = require("./projects-model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const theProjects = await Projects.get();
+    res.json(theProjects);
+  } catch (err) {
+    res.status(500).json({
+      message: "An error has happened",
+    });
+  }
+});
+
+module.exports = router;
